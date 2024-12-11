a881d49807bc45341dc1f4b71ed6c16ec4e23951
Tamana543 <tamanafarzami33@gmail.com>
2024-12-11 16:51:02 +0430

    File Updated

--- JavaScript_Projects/TestProjects.js
+++ JavaScript_Projects/TestProjects.js
@@ -4,3 +4,14 @@ let age;
 console.log(age);
 console.log(profession);
 let count  = 8;
+console.log(count + 1)
+let rows = [];
+rows = ["Naomi","Quincy","CamperChan"];
+console.log(rows[0])
+rows[2] = 10;
+console.log(rows)
+rows[rows.length-1] =3;
+let cities = ["London","New York","Mumbai"]
+console.log(cities);
+cities[cities.length-1] = "Mexico City";
+console.log(cities)
