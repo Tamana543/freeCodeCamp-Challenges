44cc41813c98e25fd19175a24c06c74cd4a1d6a3
Tamana543 <tamanafarzami33@gmail.com>
2024-12-17 16:12:17 +0430

    File updated

--- JavaScript_Projects/TestProjects.js
+++ JavaScript_Projects/TestProjects.js
@@ -58,6 +58,10 @@ let sum = addTwoNumbers(5,10);
         //   rows.push(padRow(rows.length+1,count))
          
         // }
-        for (let i = count; i > 0; i--) {
-          rows.push(padRow(i, count));
-        }
+        // for (let i = count; i > 0; i--) {
+        //   rows.push(padRow(i, count));
+        // }
+        const numbers = [1,2,3];
+        const shifted = numbers.shift();
+
+        const unshifted = numbers.unshift(5);
