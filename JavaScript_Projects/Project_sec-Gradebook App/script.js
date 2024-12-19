14b35e288ee0711c4a035b0c01a52fbac79750a8
Tamana543 <tamanafarzami33@gmail.com>
2024-12-19 16:36:32 +0430

    File Updated

--- JavaScript_Projects/Project_sec-Gradebook App/script.js
+++ JavaScript_Projects/Project_sec-Gradebook App/script.js
@@ -1,10 +1,13 @@
 function getAverage(scores) {
-     const data = scores;
-     const averrage= [];
-     for(let i = data; i<1; i++)
-   return averrage;
-   
-   }
-   
-   console.log(getAverage([92, 88, 12, 77, 57, 100, 67, 38, 97, 89]));
-   console.log(getAverage([45, 87, 98, 100, 86, 94, 67, 88, 94, 95]));
+  const data = scores;
+  const averrage= [];
+//   for(let i = data; i > 0; i++){
+//     averrage.push(i)
+//   }
+data.forEach(el => {averrage.push(el + el / data.length)});
+
+return averrage;
+}
+
+console.log(getAverage([92, 88, 12, 77, 57, 100, 67, 38, 97, 89]));
+console.log(getAverage([45, 87, 98, 100, 86, 94, 67, 88, 94, 95]));
