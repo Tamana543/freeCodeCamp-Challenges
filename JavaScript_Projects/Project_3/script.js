2b8693b9f36df58d01d02f68cfd214534e44d37d
Tamana543 <tamanafarzami33@gmail.com>
2024-12-28 16:15:36 +0430

    File Updated

--- JavaScript_Projects/Project_3/script.js
+++ JavaScript_Projects/Project_3/script.js
@@ -16,14 +16,39 @@ const goldText = document.querySelector("#goldText");
 const monsterStats = document.querySelector("#monsterStats");
 const monsterName = document.querySelector("#monsterName");
 const monsterHealthText = document.querySelector("#monsterHealth");
+function goTown(){
+     button1.innerText = "Buy 10 health (10 gold)";
+     button2.innerText = "Buy weapon (30 gold)";
+     button3.innerText = "Go to town square";
+     button1.onclick = buyHealth;
+     button2.onclick = buyWeapon;
+     button3.onclick = goTown;     
+     text.innerText = "You enter the store."; 
+     }
 function goStore(){
-  console.log("Going to store.");
+//   console.log("Going to store.");
+button1.innerText = "Buy 10 health (10 gold)";
+button2.innerText = "Buy weapon (30 gold)";
+button3.innerText = "Go to town square";
+button1.onclick = buyHealth;
+button2.onclick = buyWeapon;
+button3.onclick = goTown;     
+text.innerText = "You enter the store."; 
 }
 function goCave(){
-  console.log("Going to cave.");
+//   console.log("Going to cave.");
 }
 function fightDragon(){
-  console.log("Fighting dragon.");
+//   console.log("Fighting dragon.");
 }
 // initialize buttons
 button1.onclick = goStore;
+button2.onclick = goCave;
+button3.onclick = fightDragon;
+function buyHealth(){
+    
+     }
+function buyWeapon(){
+    
+     }
+     
