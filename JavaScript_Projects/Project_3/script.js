7a1b549d57f00ff91fccfbf9ecebe1f413c28981
Tamana543 <tamanafarzami33@gmail.com>
2024-12-29 16:15:14 +0430

    file Updated

--- JavaScript_Projects/Project_3/script.js
+++ JavaScript_Projects/Project_3/script.js
@@ -29,7 +29,8 @@ const locations = [
      }
 ];
 const monsterHealthText = document.querySelector("#monsterHealth");
-function update(location){   button1.innerText = "Go to store";
+function update(location){   
+     button1.innerText = location["button text"][0];
      button2.innerText = "Go to cave";
      button3.innerText = "Fight dragon";
      button1.onclick = goStore;
@@ -37,7 +38,7 @@ function update(location){   button1.innerText = "Go to store";
      button3.onclick = fightDragon;
      text.innerText = "You are in the town square. You see a sign that says Store."; }
 function goTown(){
-
+     update(locations[0])
          
         }
 function goStore(){
