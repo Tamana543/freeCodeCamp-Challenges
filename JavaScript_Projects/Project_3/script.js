b62450dceceecda7012b15c23b8bbc21aa2d8895
Tamana543 <tamanafarzami33@gmail.com>
2025-01-08 19:13:39 +0430

    File Updated

--- JavaScript_Projects/Project_3/script.js
+++ JavaScript_Projects/Project_3/script.js
@@ -101,9 +101,21 @@ goFight();
           text.innerText+= " You attack it with your " + weapons[currentWeaponIndex].name + ".";
           health -= monsters[fighting].level;
           monsterHealth -= weapons[currentWeaponIndex].power + Math.floor(Math.random()* xp )+1;
-         
+          healthText.innerText = health;
+          monsterHealthText.innerText = monsterHealth;
+          if(health <= 0 ){
+         lose()
+          }else if (monsterHealth <= 0 ){
+        defeatMonster();
+          }
      }
      function dodge(){
+          text.innerText = "You dodge the attack from the " + monsters[fighting].name;
+     }
+     function lose(){
+
+     }
+     function defeatMonster(){
 
      }
 function buyHealth(){
