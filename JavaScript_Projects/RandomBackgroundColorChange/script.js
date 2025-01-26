00512bac207b8518425f2b516219eca3fb1374f2
Tamana Farzami <tamanafarzami33@gmail.com>
2025-01-26 03:29:53 -0800

    File Updated

---
+++ JavaScript_Projects/RandomBackgroundColorChange/script.js
@@ -0,0 +1,21 @@
+const darkColorsArr = [
+    "#2C3E50",
+    "#34495E",
+    "#2C2C2C",
+    "#616A6B",
+    "#4A235A",
+    "#2F4F4F",
+    "#0E4B5A",
+    "#36454F",
+    "#2C3E50",
+    "#800020",
+  ];
+  const dataEl = document.querySelector(".data");
+  const btn = document.getElementById("mainBtn");
+  function colorFunction(){
+ for (let i = 0 ; i < darkColorsArr.length  ; i++){
+
+    
+}
+  }
+  btn.addEventListener("click",colorFunction)
