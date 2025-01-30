f705944d78d77e49f5eade75545fee7863898fed
Tamana Farzami <tamanafarzami33@gmail.com>
2025-01-30 04:42:32 -0800

    File Updated

--- JavaScript_Projects/ProjectFive/script.js
+++ JavaScript_Projects/ProjectFive/script.js
@@ -5,3 +5,12 @@ const addEntryButton = document.getElementById("add-entry")
 const clearButton = document.getElementById("clear")
 const output= document.getElementById("output")
 let isError = false;
+function cleanInputString(str){
+    // In regex, shorthand character classes allow you to match specific characters without having to write those characters in your pattern. Shorthand character classes are preceded with a backslash (\). The character class \s will match any whitespace character.
+//   To tell the pattern to match each of these characters individually, you need to turn them into a character class. This is done by wrapping the characters you want to match in brackets
+//  Regex can also take specific flags to alter the pattern matching behavior. Flags are added after the closing /. The g flag, which stands for "global", will tell the pattern to continue looking after it has found a match.
+    // const regex = /[\+-\s]/;
+    const regex = /[+-\s]/g;
+    return str.replace(regex,"")
+
+}
