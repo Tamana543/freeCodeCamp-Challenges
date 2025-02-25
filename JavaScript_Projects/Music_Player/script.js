723dd8e59b2f065130ebf553fe01c8653be374e5
Tamana Farzami <tamanafarzami33@gmail.com>
2025-02-25 03:19:17 -0800

    Challenge 7-11  Solved

--- JavaScript_Projects/Music_Player/script.js
+++ JavaScript_Projects/Music_Player/script.js
@@ -70,3 +70,9 @@ src: "https://cdn.freecodecamp.org/curriculum/js-music-player/still-learning.mp3
     src: "https://cdn.freecodecamp.org/curriculum/js-music-player/chasing-that-feeling.mp3",
   },
 ];
+const audio = new Audio();
+let userData = {
+    "songs" : [...allSongs],
+    "currentSong" : null ,
+    "songCurrentTime" : 0
+};
