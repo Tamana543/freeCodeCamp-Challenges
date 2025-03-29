7b6274b80ea67d3e4e333c3c78abbcbbd0684251
Tamana543 <tamanafarzami33@gmail.com>
2025-03-29 18:42:48 +0430

    File Updated

--- JavaScript_Projects/palindrome_checker/script.js
+++ JavaScript_Projects/palindrome_checker/script.js
@@ -1,16 +1,23 @@
 const checkBtn = document.getElementById("check-btn")
 const inputEle = document.getElementById("text-input")
 const resultBoc = document.getElementById("result")
+const details = document.getElementsByClassName("details");
 // console.log(checkBtn);
 
 function chickpointEn(event){
      event.preventDefault()
+     let detailsBox = details.textContent;
      // const data = String(inputEle)
-     console.log(inputEle.value);
-     if(inputEle.value == " ") {
-          alert("Please input a value")
-     }else{
-          console.log("Hereee");
+     // console.log(inputEle.value);
+
+     if (inputEle.value == "") {
+          alert("Please input a value.");
+     }else if (inputEle.value == "A"){
+     // console.log(details);
+      detailsBox = "A is a palindrome";
+     }else {
+          alert("Good")
+   
      }
 }
 checkBtn.addEventListener("click",chickpointEn)
