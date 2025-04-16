0627f42d50f68d71843b715099dbf3811bb28a50
Tamana543 <tamanafarzami33@gmail.com>
2025-04-16 15:59:11 +0430

    file updated

--- JavaScript_Projects/decimal-to-binaryConverter/script.js
+++ JavaScript_Projects/decimal-to-binaryConverter/script.js
@@ -7,5 +7,7 @@ function checkUserInput(){
 }
 convertBtn.addEventListener("click",checkUserInput)
 numberInput.addEventListener("keydown",(e)=>{
-console.log(e);
+if(e.key === "Enter") {
+     checkUserInput()
+}
 })
