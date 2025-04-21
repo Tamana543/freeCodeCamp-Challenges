c1d80931d7e368c6a91afd3d73a501e9bef39550
Tamana543 <tamanafarzami33@gmail.com>
2025-04-21 15:57:31 +0430

    Animation Functionality Developed

--- JavaScript_Projects/decimal-to-binaryConverter/script.js
+++ JavaScript_Projects/decimal-to-binaryConverter/script.js
@@ -6,14 +6,23 @@ const animationData = [
      {
           inputVal : 5,
           addElDelay : 1000,
+          msg:"decimalToBinary(5) returns '10' + 1 (5 % 2). Then it pops off the stack.",
+          showMsgDelay : 15000,
+          removeElDelay : 20000,
      },
      {
           inputVal : 2,
-          addElDelay: 1500
+          addElDelay: 1500,
+          msg:"decimalToBinary(2) returns '1' + 0 (2 % 2) and gives that value to the stack below. Then it pops off the stack.",
+          showMsgDelay : 10000,
+          removeElDelay : 15000,
      } ,
      {
           inputVal : 1,
-          addElDelay: 2000
+          addElDelay: 2000,
+          msg:"decimalToBinary(1) returns '1' (base case) and gives that value to the stack below. Then it pops off the stack.",
+          showMsgDelay : 5000,
+          removeElDelay : 10000,
      },
 
 ];
@@ -75,7 +84,12 @@ function decimalToBinaryThirdway(input){
      }
 }
 function showAnimation(){
-   
+   result.innerText = "Call Stack Animation"
+   animationData.forEach((obj)=>{
+  setTimeout(() => {
+     animationContainer.innerHTML += `<p id="${obj.inputVal} class="animation-frame">decimalToBinary(${obj.inputVal})</p>`
+  }, obj.addElDelay);
+   })
 }
 convertBtn.addEventListener("click",checkUserInput)
 numberInput.addEventListener("keydown",(e)=>{
