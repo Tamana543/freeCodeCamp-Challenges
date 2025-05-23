de4306dec775e96beb502a75611fc11df5fdc0a2
Tamana543 <tamanafarzami33@gmail.com>
2025-05-23 14:47:18 +0430

    Challenge 5-8 solved

--- JavaScript_Projects/spamMessageChecker/script.js
+++ JavaScript_Projects/spamMessageChecker/script.js
@@ -2,8 +2,9 @@ const messageInput = document.getElementById("message-input")
 const result = document.getElementById("result")
 const checkMessageButton = document.getElementById("check-message-btn")
 
-const helpRegex = /please help| assist me/i // here
-const isSpam = (msg)=>  helpRegex.test(msg)
+const helpRegex = /please help|assist me/i // here
+const denyList = [helpRegex]
+const isSpam = (msg)=> denyList.some(regex => regex.test(msg))
 checkMessageButton.addEventListener("click",()=>{
      if(messageInput.value === "") {
           alert("Please enter a message.")
