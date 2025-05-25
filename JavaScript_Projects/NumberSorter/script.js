51fb8db69c3930059f3883ef4b6de01fd8acf8f6
Tamana543 <tamanafarzami33@gmail.com>
2025-05-25 17:32:58 +0430

    Functionality Updated

--- JavaScript_Projects/NumberSorter/script.js
+++ JavaScript_Projects/NumberSorter/script.js
@@ -1,8 +1,19 @@
 const sortButton = document.getElementById("sort")
+
+const updateUI = (array=[])=>{
+     array.forEach((num,i)=>{
+          const outputValueNode = document.getElementById(`output-value-${i}`)
+          outputValueNode.innerText = num;
+     })
+}
 const sortInputArray = (event)=>{
      event.preventDefault()
      const inputValues = [...document.getElementsByClassName("values-dropdown")].map(dropdown=>Number(dropdown.value));
+     updateUI(inputValues)
      // console.log(inputValues);
+}
+const bubbleSort = (array)=>{
+
 }
 
 sortButton.addEventListener("click",sortInputArray)
