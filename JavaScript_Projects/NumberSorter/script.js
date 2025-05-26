27ccaa5547d150591e7e61467fcebf0a98113f0e
Tamana543 <tamanafarzami33@gmail.com>
2025-05-26 16:06:48 +0430

    functionality updated

--- JavaScript_Projects/NumberSorter/script.js
+++ JavaScript_Projects/NumberSorter/script.js
@@ -11,13 +11,13 @@ const sortInputArray = (event)=>{
      const inputValues = [...document.getElementsByClassName("values-dropdown")].map(dropdown=>Number(dropdown.value));
      updateUI(inputValues)
      // console.log(inputValues);
-     const sortedValues = bubbleSort(inputValues)
+     const sortedValues = selectionSort(inputValues)
      updateUI(sortedValues)
 }
 const bubbleSort = (array)=>{
      for(let i = 0 ; i < array.length ; i++){
           for (let j = 0 ; j < array.length-1 ; j++){
-               console.log(array,array[j],array[j+1]);
+               // console.log(array,array[j],array[j+1]);
                if(array.j > array.j+1){
                     const temp = array[j]
                     array[j]=array[j+1]
@@ -27,5 +27,21 @@ const bubbleSort = (array)=>{
 }
 return array;
 }
+const selectionSort = (array)=>{
+for(let i = 0; i < array.length;i++ ){
+     let minIndex = i;
 
+      for(let j = i+1 ; j < array.length; j++){
+          // console.log(array,array[j],array[minIndex]);
+          if(array[j]< array[minIndex]){
+               minIndex = j
+          }
+     }
+     const temp = array[i];
+      array[i] = array[minIndex]
+      array[minIndex] = temp;
+}
+return array
+}
+const insertionSort = (array)=>{}
 sortButton.addEventListener("click",sortInputArray)
