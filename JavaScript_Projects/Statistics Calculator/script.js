11f9c951710cca15792ec6b9ee4a3babf73ccfcf
Tamana543 <tamanafarzami33@gmail.com>
2025-05-28 15:46:19 +0430

    Coculation : mean function added

--- JavaScript_Projects/Statistics Calculator/script.js
+++ JavaScript_Projects/Statistics Calculator/script.js
@@ -1,5 +1,7 @@
 const container = document.querySelector(".container")
 const hiddenText = document.getElementById("learnMore")
+
+// UI functionality 
 function displayTextEng(){
 hiddenText.classList.remove("hidden")
 container.classList.add("hidden")
@@ -8,4 +10,17 @@ console.log(container);
 function hideTextEng(){
      hiddenText.classList.add("hidden")
 container.classList.remove("hidden")
+}
+
+// main Functionality
+const calculate = function(){
+       const value = document.querySelector("#numbers").value;
+       const array = value.split(/,\s*/g);
+       const numbers = array.map(el => Number(el)).filter(el => !isNaN(el));
+};
+
+function getMean(array){
+        const sum = array.reduce((acc,el)=> acc + el,0)
+        const mean = sum / array.length
+        return mean
 }
