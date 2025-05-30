5452e4443e4dd8968d12d2880cfbfb8fc35ccadd
Tamana543 <tamanafarzami33@gmail.com>
2025-05-30 16:11:18 +0430

    mode functionality completed

--- JavaScript_Projects/Statistics Calculator/script.js
+++ JavaScript_Projects/Statistics Calculator/script.js
@@ -11,9 +11,6 @@ function hideTextEng(){
      hiddenText.classList.add("hidden")
 container.classList.remove("hidden")
 }
-
-
-
 const getMean = (array) => array.reduce((acc, el) => acc + el, 0) / array.length;
 // const testArr1 = [1, 2, 3, 4, 5];
 // const testArr2 = [1, 2, 3, 4, 5, 6];
@@ -31,9 +28,13 @@ const getMedian = (array)=>{
 }
 const getMode = (array)=>{
   const counts = {};
-  array.forEach(counts[el]?counts[el]++ : counts[el]=1)
-    
-  return counts
+  array.forEach(el => counts[el] = counts[el] ? counts[el] + 1 : 1)
+  if(new Set(Object.values(counts)).size === 1){
+    return null
+  }
+  const highest = Object.keys(counts).sort((a , b)=> counts[b]- counts[a])[0]
+  const mode = Object.keys(counts).filter(el => counts[el] === counts[highest])
+  return mode.join(", ")
 }
 
 // main Functionality
@@ -43,7 +44,9 @@ const calculate = function(){
        const numbers = array.map(el => Number(el)).filter(el => !isNaN(el));
        const mean = getMean(numbers)
        const median = getMedian(numbers)   
+       const mode = getMode(numbers);
            // Volues assigning
       document.querySelector("#mean").textContent = mean
       document.querySelector("#median").textContent = median
+      document.querySelector("#mode").textContent = mode
 };
