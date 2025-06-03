51ad5c6fcea3fb412fd13814aa2d92e92080473b
Tamana543 <tamanafarzami33@gmail.com>
2025-06-03 14:48:37 +0430

    the regex added

--- JavaScript_Projects/spreadSheetByJs/script.js
+++ JavaScript_Projects/spreadSheetByJs/script.js
@@ -21,7 +21,8 @@ const spreadsheetFunctions = {
 }
 // to parse and evauate the input 
 const evalFormula = (x , cells)=>{
-     const idToText = (id)=> cells.find(cell => cell.id===id).value
+     const idToText = (id)=> cells.find(cell => cell.id===id).value;
+        const rangeRegex = /([A-J])([1-9][0-9]?):([A-J])([1-9][0-9]?)/gi;
 }
 window.onload = () => {
   const container = document.getElementById("container");
