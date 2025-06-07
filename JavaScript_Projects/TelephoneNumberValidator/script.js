a1164aa1be4bbdc688bfa0a334099e57c81b6b80
Tamana543 <tamanafarzami33@gmail.com>
2025-06-07 09:12:46 +0430

    script slection added

--- JavaScript_Projects/TelephoneNumberValidator/script.js
+++ JavaScript_Projects/TelephoneNumberValidator/script.js
@@ -1,2 +1,5 @@
-
+const chickBtn = document.getElementById("check-btn");
+const clearBtn = document.getElementById("clear-btn");
+const userInput = document.getElementById("user-input");
+const resultDiv = document.getElementById("result-div");
 
