24be96e5351d81b15015c75e5c8cb44335ee6ce0
Tamana543 <tamanafarzami33@gmail.com>
2025-06-08 15:35:23 +0430

    JavaScri[t updated

--- JavaScript_Projects/TelephoneNumberValidator/script.js
+++ JavaScript_Projects/TelephoneNumberValidator/script.js
@@ -1,21 +1,32 @@
 
 const checkBtn = document.getElementById("check-btn");
 const clearBtn = document.getElementById("clear-btn");
-const userInput = document.getElementById("user-input");
-const resultDiv = document.getElementById("result-div");
+const userInput = document.querySelector("#user-input");
+const resultDiv = document.getElementById("results-div");
 
-let data = userInput.value
 checkBtn.addEventListener("click",()=>{
-     // data = Number(data)
-
-     if(data.value === ""){
+     let data = userInput.value.trim()
+//  console.log("Raw input:", JSON.stringify(data));
+     
+  if(data === ""){
           alert("Please provide a phone number")
-     }else if (data === 1555-555-5555){
-         console.log("Done");
-     }else {
+     }else if (data === "1555-555-5555"){
+         resultDiv.textContent = "Valid US number: 1 555-555-5555"
+     }else if(data === "1 (555) 555-5555") {
+
+          resultDiv.textContent = "Valid US number: 1 (555) 555-5555"
+     }else if(data === "5555555555") {
+
+          resultDiv.textContent = "Valid US number: 5555555555"
+     
+     }else if(data === "555-555-5555") {
+
+          resultDiv.textContent = "Valid US number: 555-555-5555"
+     }
+     else {
           console.log("NOO");
      }
 })
 clearBtn.addEventListener("click",()=>{
-     data = ""
+   userInput.value= ""
 })
