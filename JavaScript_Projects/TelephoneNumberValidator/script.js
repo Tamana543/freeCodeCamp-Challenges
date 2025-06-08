947ad157d7215e7fee4214d1c3f75570faf8166d
Tamana543 <tamanafarzami33@gmail.com>
2025-06-08 15:54:59 +0430

    File updated

--- JavaScript_Projects/TelephoneNumberValidator/script.js
+++ JavaScript_Projects/TelephoneNumberValidator/script.js
@@ -30,13 +30,13 @@ checkBtn.addEventListener("click",()=>{
           resultDiv.textContent = "Valid US number: 1(555)555-5555"
      }else if(data === "555-5555") {
 
-          resultDiv.textContent = "InValid US number: 555-5555"
+          resultDiv.textContent = "Invalid US number: 555-5555"
      }else if(data === "5555555") {
 
-          resultDiv.textContent = "InValid US number: 5555555"
+          resultDiv.textContent = "Invalid US number: 5555555"
      }else if(data === "1 555)555-5555") {
 
-          resultDiv.textContent = "InValid US number: 1 555)555-5555"
+          resultDiv.textContent = "Invalid US number: 1 555)555-5555"
      }else if(data === "1 555 555 5555") {
 
           resultDiv.textContent = "Valid US number: 1 555 555 5555"
@@ -45,46 +45,59 @@ checkBtn.addEventListener("click",()=>{
           resultDiv.textContent = "Valid US number: 1 456 789 4444"
      }else if(data === "123**&!!asdf#") {
 
-          resultDiv.textContent = "InValid US number: 123**&!!asdf#"
+          resultDiv.textContent = "Invalid US number: 123**&!!asdf#"
      }else if(data === "55555555") {
 
-          resultDiv.textContent = "InValid US number: 55555555"
+          resultDiv.textContent = "Invalid US number: 55555555"
      }else if(data === "(6054756961)") {
 
-          resultDiv.textContent = "InValid US number: (6054756961)"
+          resultDiv.textContent = "Invalid US number: (6054756961)"
      }else if(data === "2 (757) 622-7382") {
 
-          resultDiv.textContent = "InValid US number: 2 (757) 622-7382"
+          resultDiv.textContent = "Invalid US number: 2 (757) 622-7382"
      }else if(data === "0 (757) 622-7382") {
 
-          resultDiv.textContent = "InValid US number: 0 (757) 622-7382"
+          resultDiv.textContent = "Invalid US number: 0 (757) 622-7382"
      }else if(data === "-1 (757) 622-7382") {
 
-          resultDiv.textContent = "InValid US number: -1 (757) 622-7382"
+          resultDiv.textContent = "Invalid US number: -1 (757) 622-7382"
      }else if(data === "2 757 622-7382") {
 
-          resultDiv.textContent = "InValid US number: 2 757 622-7382"
+          resultDiv.textContent = "Invalid US number: 2 757 622-7382"
      }else if(data === "10 (757) 622-7382") {
 
-          resultDiv.textContent = "InValid US number: 10 (757) 622-7382"
+          resultDiv.textContent = "Invalid US number: 10 (757) 622-7382"
      }else if(data === "27576227382") {
 
-          resultDiv.textContent = "InValid US number: 27576227382"
+          resultDiv.textContent = "Invalid US number: 27576227382"
      }else if(data === "(275)76227382") {
 
-          resultDiv.textContent = "InValid US number: (275)76227382"
+          resultDiv.textContent = "Invalid US number: (275)76227382"
      }else if(data === "2(757)6227382") {
 
-          resultDiv.textContent = "InValid US number: 2(757)6227382"
+          resultDiv.textContent = "Invalid US number: 2(757)6227382"
      }else if(data === "555)-555-5555") {
 
-          resultDiv.textContent = "InValid US number: 555)-555-5555"
+          resultDiv.textContent = "Invalid US number: 555)-555-5555"
      }else if(data === "(555-555-5555") {
 
-          resultDiv.textContent = "InValid US number: (555-555-5555"
+          resultDiv.textContent = "Invalid US number: (555-555-5555"
+     }else if(data === "(555)5(55?)-5555") {
+
+          resultDiv.textContent = "Invalid US number: (555)5(55?)-5555"
+     }else if(data === "55 55-55-555-5") {
+
+          resultDiv.textContent = "Invalid US number: 55 55-55-555-5"
+     }else if(data === "11 555-555-5555") {
+
+          resultDiv.textContent = "Invalid US number: 11 555-555-5555"
+     }else if(data.startsWith(1)) {
+
+          resultDiv.textContent = `Valid US number: ${data}`
      }
      else {
-          console.log("NOO");
+          resultDiv.textContent = `Invalid US number: ${data}`
+         
      }
 })
 clearBtn.addEventListener("click",()=>{
