65e86bb678c1b818cbfeff05321aeeb3e3e8295b
Tamana543 <tamanafarzami33@gmail.com>
2025-06-08 15:06:14 +0430

    javaScript Updated

--- JavaScript_Projects/TelephoneNumberValidator/script.js
+++ JavaScript_Projects/TelephoneNumberValidator/script.js
@@ -1,5 +1,21 @@
-const chickBtn = document.getElementById("check-btn");
+
+const checkBtn = document.getElementById("check-btn");
 const clearBtn = document.getElementById("clear-btn");
 const userInput = document.getElementById("user-input");
 const resultDiv = document.getElementById("result-div");
 
+let data = userInput.value
+checkBtn.addEventListener("click",()=>{
+     // data = Number(data)
+
+     if(data.value === ""){
+          alert("Please provide a phone number")
+     }else if (data === 1555-555-5555){
+         console.log("Done");
+     }else {
+          console.log("NOO");
+     }
+})
+clearBtn.addEventListener("click",()=>{
+     data = ""
+})
