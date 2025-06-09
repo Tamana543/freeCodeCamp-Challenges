a7ec44686ddd10d194ac387ed607352a311b6a5a
Tamana543 <tamanafarzami33@gmail.com>
2025-06-09 14:43:31 +0430

    function updated

--- JavaScript_Projects/TelephoneNumberValidator/regexJs.js
+++ JavaScript_Projects/TelephoneNumberValidator/regexJs.js
@@ -26,17 +26,9 @@ clearBtn.addEventListener("click", () => {
 
 // VALIDATOR FUNCTION / work more !!!!
 
+
+
 function isValidUSPhoneNumber(str) {
-  const pattern = new RegExp(
-    '^([+]?1[\\s]?)?' + // optional country code
-    '(' +
-      '(\\([2-9]1[02-9]\\)|\\([2-9][02-8][0-9]\\)[\\s]?)' + // area code in parentheses
-      '|' +
-      '([2-9]1[02-9]|[2-9][02-8][0-9])[\\s.-]?' + // or area code without parentheses
-    ')' +
-    '([2-9]1[02-9]|[2-9][02-9]1|[2-9][02-9]{2})[\\s.-]?' + // valid exchange code
-    '([0-9]{4})$' // last 4 digits
-  );
+  const pattern =  [/^1? ?(( ?\d{3} [- ]*)|(\( ?\d{3}[- ]*\) *))\d{3}[- ]?\d{4}$/gm, /\d{10}/gm, /\d*\(\d[\d)]{3}\d{3}-\d{4}/gm, /\d{3}-\d{3}-\d{4}/gm, /^\d\s\d{3}-\d{3}-\d{4}/gm];
   return pattern.test(str);
 }
-// /^1? ?(( ?\d{3} [- ]*)|(\( ?\d{3}[- ]*\) *))\d{3}[- ]?\d{4}$/gm, /\d{10}/gm, /\d*\(\d[\d)]{3}\d{3}-\d{4}/gm, /\d{3}-\d{3}-\d{4}/gm, /^\d\s\d{3}-\d{3}-\d{4}/gm
