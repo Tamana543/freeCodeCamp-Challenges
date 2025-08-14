d1d9606cd4ea9bedcad3502982b38ecef4c44890
Tamana543 <tamanafarzami33@gmail.com>
2025-08-14 20:38:32 +0430

    File Updated

--- JavaScript_Projects/block_Game/script.js
+++ JavaScript_Projects/block_Game/script.js
@@ -18,5 +18,23 @@ let isCheckpointCollisionDetectionActive = true;
  As you are designing the game, you will need to make sure that the size of the elements in the game are responsive and adapt to different screen sizes.
  */
 const proportionalSize =(size)=>{
-     
+     return innerHeight<500 ? Math.ceil((size / 500) * innerHeight) : size
 } ;
+class Player {
+     constructor() {
+          this.position = {
+               x : proportionalSize(10),
+               y:proportionalSize(400)
+          }
+
+          this.velocity = {
+               x : 0,
+               y:0
+          }
+
+          // width and height properties of your class to be proportional to the height of the screen.
+          this.width =proportionalSize(40)
+          this.height = proportionalSize(40)
+
+     }
+}
