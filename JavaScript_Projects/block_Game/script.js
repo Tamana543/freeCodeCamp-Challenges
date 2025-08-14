93757d437287ebb4e12897bec4fb084045a4d460
Tamana543 <tamanafarzami33@gmail.com>
2025-08-14 20:31:46 +0430

    file updated

--- JavaScript_Projects/block_Game/script.js
+++ JavaScript_Projects/block_Game/script.js
@@ -2,3 +2,21 @@ const startBtn = document.getElementById('start-btn');
 const canvas = document.getElementById('canvas');
 const startScreen  = document.querySelector('.start-screen');
 const checkpointScreen = document.querySelector('.checkpoint-screen');
+const checkpointMessage = document.querySelector('.checkpoint-screen > p');
+
+/**
+ The Canvas API can be used to create graphics in games using JavaScript and the HTML canvas element.
+
+You will need to use the getContext method which will provide the context for where the graphics will be rendered.
+ */
+const ctx = canvas.getContext("2d");
+canvas.width = innerWidth
+canvas.height = innerHeight
+const gravity =0.5 ;
+let isCheckpointCollisionDetectionActive = true;
+/**
+ As you are designing the game, you will need to make sure that the size of the elements in the game are responsive and adapt to different screen sizes.
+ */
+const proportionalSize =(size)=>{
+     
+} ;
