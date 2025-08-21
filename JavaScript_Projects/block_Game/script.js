9a93acb5242de7fd7a9becad1fd3829e0c5295ca
Tamana543 <tamanafarzami33@gmail.com>
2025-08-21 21:24:30 +0430

    platform position object added

--- JavaScript_Projects/block_Game/script.js
+++ JavaScript_Projects/block_Game/script.js
@@ -68,7 +68,20 @@ class Player {
 
 }
 const player = new Player();
-const platformPositions =[]
+const platformPositions =[
+{ x: 500, y: proportionalSize(450) },
+  { x: 700, y: proportionalSize(400) },
+  { x: 850, y: proportionalSize(350) },
+  { x: 900, y: proportionalSize(350) },
+  { x: 1050, y: proportionalSize(150) },
+  { x: 2500, y: proportionalSize(450) },
+  { x: 2900, y: proportionalSize(400) },
+  { x: 3150, y: proportionalSize(350) },
+  { x: 3900, y: proportionalSize(450) },
+  { x: 4200, y: proportionalSize(400) },
+  { x: 4400, y: proportionalSize(200) },
+  { x: 4700, y: proportionalSize(150) },
+]
 const startGame = ()=>{
      canvas.style.display = "block";
      startScreen.style.display = "none"
@@ -76,6 +89,7 @@ const startGame = ()=>{
 
 }
 startBtn.addEventListener('click',startGame)
+const platforms = platformPositions.map()
 const animate = ()=>{
      requestAnimationFrame(animate)//requestAnimationFrame() web API, takes in a callback and is used to update the animation on the screen. The animate function will be responsible for updating the player's position and continually drawing it on the canvas.
      // first clear the canvas to move the player
