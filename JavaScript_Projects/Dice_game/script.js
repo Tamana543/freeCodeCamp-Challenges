953fc6e3265808b6f18a3c7363d5b6d59f40fd65
Tamana543 <tamanafarzami33@gmail.com>
2025-08-25 15:25:58 +0430

    file updated

--- JavaScript_Projects/Dice_game/script.js
+++ JavaScript_Projects/Dice_game/script.js
@@ -1,18 +1,19 @@
-const listOfAllDice= document.querySelectorAll(".die");
-const scoreInputs = document.querySelector("#score-options") ;
-const scoreSpans = document.querySelector("span");
-const roundElement = document.querySelector("#current-round");
-const rollsElement = document.querySelector("#current-round-rolls");
-const totalScore = document.querySelector("#total-score");
-const scoreHistory = document.querySelector("#score-history");
-const rollDiceBtn = document.querySelector(" #roll-dice-btn");
-const keepScoreBtn = document.querySelector("#keep-score-btn")
-const rulesBtn= document.querySelector("#rules-btn")
-const rulesContainer = document.querySelector(".rules-container")
+const listOfAllDice = document.querySelectorAll(".die");
+const scoreInputs = document.querySelectorAll("#score-options input");
+const scoreSpans = document.querySelectorAll("#score-options span");
+const roundElement = document.getElementById("current-round");
+const rollsElement = document.getElementById("current-round-rolls");
+const totalScoreElement = document.getElementById("total-score");
+const scoreHistory = document.getElementById("score-history");
+const rollDiceBtn = document.getElementById("roll-dice-btn");
+const keepScoreBtn = document.getElementById("keep-score-btn");
+const rulesBtn = document.getElementById("rules-btn");
+const rulesContainer = document.querySelector(".rules-container");
 
-let rolls;
-let score;
-let round;
-let diceValuesArr;
+let rolls = 0;
+let score = 0;
+let round = 1;
+let diceValuesArr = [];
+let isModalShowing = false;
 
-
+rulesBtn.addEventListener("click")
