151ea44fd33c81847f9ee1f3f7a388170d91f6fc
Tamana543 <tamanafarzami33@gmail.com>
2025-08-30 16:42:31 +0430

    file updated

--- JavaScript_Projects/cashRegisterPro/script.js
+++ JavaScript_Projects/cashRegisterPro/script.js
@@ -1,5 +1,6 @@
 const cushEle =document.getElementById("cash") ;
 const submitBtn = document.getElementById("purchase-btn");
+const changeDue = document.getElementById("change-due")
 
 let price = 1.87;
 let cid = [
@@ -13,12 +14,19 @@ let cid = [
   ['TWENTY', 60],
   ['ONE HUNDRED', 100]
 ];
+function mainFunc(event){
+     event.preventDefault()
+     const cushValue = cushEle.value
+     if(cushValue < price){
+          alert("Customer does not have enough money to purchase the item")
+     }else if (cushValue == price){
+     changeDue.textContent="No change due - customer paid with exact cash"
+     }else {
+          
+          console.log("me");
+     }
+     console.log(cushValue);
+     cushEle.value = ""
 
-submitBtn.addEventListener('click',(event)=>{
-event.preventDefault()
-const cushValue = cushEle.value
-
-
-console.log(cushValue);
-cushEle.value = ""
-})
+}
+submitBtn.addEventListener("click",mainFunc)
