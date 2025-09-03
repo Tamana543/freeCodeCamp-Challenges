66c689cd8b84838a5a84c0832ff23688d77b18f2
Tamana543 <tamanafarzami33@gmail.com>
2025-09-03 16:54:55 +0430

    file updated

--- JavaScript_Projects/FCC_authorPage/script.js
+++ JavaScript_Projects/FCC_authorPage/script.js
@@ -5,7 +5,10 @@ fetch(
 "https://cdn.freecodecamp.org/curriculum/news-author-page/authors.json"
 )
 .then(res=>res.json()).then(data=>{
-     console.log(data);
+     // console.log(data);
+     authorDataArr =data
+     // console.log("Author Data Array:",authorDataArr);
+     displayAuthors(authorDataArr.slice(startingIndex,endingIndex))
 }).catch(err=>console.log(`There was an error: ${err}`))
 
 let startingIndex= 0;
@@ -17,6 +20,9 @@ authors.forEach(([author,image,url,bio],index)=> {
      authorContainer.innerHTML+=`
      <div id="${index}" class="user-card">
      <h2 class="author-name">${author}</h2>
+      <img src="${image}" alt="${author} avatar" class="user-img">
+      <p class="bio">${bio}</p>
+       <a href="${url}" target="_blank" class="author-link">${author}'s author page</a>
      </div>
      `
 });
