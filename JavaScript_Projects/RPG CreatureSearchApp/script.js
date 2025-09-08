dfc6c4f9b30888935eb88eaadeed63d3e3be10c6
Tamana543 <tamanafarzami33@gmail.com>
2025-09-08 11:58:59 +0430

    file updated

---
+++ JavaScript_Projects/RPG CreatureSearchApp/script.js
@@ -0,0 +1,13 @@
+const searchBtn = document.getElementById("search-button") ;
+const searchInput = document.getElementById("search-input");
+const creatureName = document.getElementById("creature-name");
+const creatureID = document.getElementById("creature-id");
+const weight = document.getElementById("weight");
+const height = document.getElementById("height");
+const hp = document.getElementById("hp");
+const attack = document.getElementById("attack");
+const defense = document.getElementById("defense");
+const specialAttack = document.getElementById("special-attack");
+const specialDefence = document.getElementById("special-defense");
+const speed = document.getElementById("speed");
+
